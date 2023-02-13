eeecad7039b3e7b543bbc350e24c0b6665d7be16
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2023-02-13 22:12:38 +0530

    Create typewise-alert.cpp

---
+++ typewise-alert.cpp
@@ -0,0 +1,70 @@
+#include "typewise-alert.h"
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+  if(value < lowerLimit) {
+    return TOO_LOW;
+  }
+  if(value > upperLimit) {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
+
+BreachType classifyTemperatureBreach(
+    CoolingType coolingType, double temperatureInC) {
+  int lowerLimit = 0;
+  int upperLimit = 0;
+  switch(coolingType) {
+    case PASSIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 35;
+      break;
+    case HI_ACTIVE_COOLING:
+      lowerLimit = 0;
+      upperLimit = 45;
+      break;
+    default:
+      lowerLimit = 0;
+      upperLimit = 40;
+      break;
+  }
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
+
+void checkAndAlert(
+    AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
+
+  BreachType breachType = classifyTemperatureBreach(
+    batteryChar.coolingType, temperatureInC
+  );
+
+  switch(alertTarget) {
+    case TO_CONTROLLER:
+      sendToController(breachType);
+      break;
+    case TO_EMAIL:
+      sendToEmail(breachType);
+      break;
+  }
+}
+
+void sendToController(BreachType breachType) {
+  const unsigned short header = 0xfeed;
+  std::cout << header << " : " << breachType << std::endl;
+}
+
+void sendToEmail(BreachType breachType) {
+  const char* recepient = "a.b@c.com";
+  switch(breachType) {
+    case TOO_LOW:
+      std::cout << "To: " << recepient << std::endl;
+      std::cout << "Hi, the temperature is too low" << std::endl;
+      break;
+    case TOO_HIGH:
+      std::cout << "To: " << recepient << std::endl;
+      std::cout << "Hi, the temperature is too high" << std::endl;
+      break;
+    default:
+      break;
+  }
+}
